cd0c1001f64eb974229a1b79c06b76ef648be3b5
ankita <ankitabksc123@gmail.com>
2022-11-10 12:55:13 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import { carsData } from "./Cars.js";
+import { settings } from './settings';
+import { useState } from 'react';
 
 function App() {
+  const [ term, setTerm]=useState("");
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='container'>
+    <input type="text" value={term} placeholder="search model here....."  onChange={(e)=>setTerm(e.target.value) }/>
     </div>
+
+    <div className="App">
+     
+      <Slider {...settings}>
+      {carsData
+      .filter((item)=>{
+        if(term===""){
+          return item;
+        }else if(item.id.toLowerCase().includes(term.toLowerCase())){
+          return item;
+        }
+      })
+      .map(item=>(
+      <div className="card">
+        <div className="card-top">
+          <span>{item.bodyType}</span>
+          <h1>{item.modelName}</h1><p>{item.modelType}</p>
+          <img src={item.imageUrl} alt={item.modelName}/>
+        </div>
+        <div className="card-bottom">
+          <div className='bottom'>
+          <span className='bottom-layer' >LEARN<a href={item.url}> <img className="arrowImage" alt='arrow' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5oesJn9vG1AnqhGRN-69YFDS1Vh9MR1pRew&usqp=CAU' /></a></span>      
+          <span className='bottom-layer'>SHOP<a href={item.shop}><img className='arrowImage' alt='arrow' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5oesJn9vG1AnqhGRN-69YFDS1Vh9MR1pRew&usqp=CAU"/></a></span>
+          </div>
+        </div>
+      </div>
+      ))}
+      </Slider>
+    </div>
+    </>
   );
 }
 
---
+++ src/Cars.js
@@ -0,0 +1,75 @@
+export const carsData = [
+
+    {
+      "id": "xc90-recharge",
+      "modelName": "XC90 Recharge", 
+      "bodyType": "suv",
+      "modelType": "plug-in hybrid",
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu4B5KAsJ5aDbSeo1Q-P2MEw3NyfV9P9cpiA&usqp=CAU",
+      "url":"https://www.volvocars.com/intl/v/cars/xc90-hybrid",
+      "shop": "https://www.volvocars.com/au/v/cars/xc90-hybrid/shop"
+    },
+    {
+      "id": "xc60-recharge",
+      "modelName": "XC60 Recharge", 
+      "bodyType": "suv",
+      "modelType": "plug-in hybrid",
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSXd6TT0S7klfawrsxIDQMxzDtashi5_AwXQ&usqp=CAU",
+       "url": "https://www.volvocars.com/en-ca/cars/xc60-hybrid/",
+       "shop": "https://www.volvocars.com/ie/v/cars/xc60-hybrid/shop"
+    },
+    {
+      "id": "xc40-recharge",
+      "modelName": "XC40 Recharge", 
+      "bodyType": "suv",
+      "modelType": "plug-in hybrid",
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY-zTEuxZRdL5LOrgT301Yp3Dw5_T5nnfx3g&usqp=CAU",
+      "url" : "https://www.volvocars.com/in/cars/xc40-electric/",
+      "shop": "https://www.volvocars.com/in/v/carsmy22/xc40-hybrid/shop"
+    },
+    {
+      "id": "xc40-bev",
+      "modelName": "XC40 Recharge", 
+      "bodyType": "suv",
+      "modelType": "pure electric",
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSusrDn-4DB9dbHiaja4PHNUGargQD5TNRgiA&usqp=CAU",
+      "url" : "https://www.volvocars.com/intl/v/cars/xc40-electric",
+      "shop": "https://www.volvocars.com/uk/shop/v90/"
+    },
+    {
+      "id": "v90-recharge",
+      "modelName": "V90 Recharge", 
+      "bodyType": "estate",
+      "modelType": "plug-in hybrid",
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSXd6TT0S7klfawrsxIDQMxzDtashi5_AwXQ&usqp=CAU",
+      "url": "https://www.volvocars.com/uk/cars/v90-hybrid/",
+      "shop": "https://www.volvocars.com/uk/shop/v90/"
+    },
+    {
+      "id": "v60-recharge",
+      "modelName": "V60 Recharge", 
+      "bodyType": "estate",
+      "modelType": "plug-in hybrid",
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk_xZkZ6bT9m5XL5WBykynKdV5K4BQ0WKmBQ&usqp=CAU",
+      "url" : "https://www.volvocars.com/us/cars/v60-hybrid/",
+      "shop": "https://www.volvocars.com/en-th/v/cars/v60-hybrid/shop"
+    },
+    {
+      "id": "s90-recharge",
+      "modelName": "S90 Recharge", 
+      "bodyType": "sedan",
+      "modelType": "plug-in hybrid",
+      "imageUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTogrQ1gnquC_xWRjQOgCl8mCvdRz52IP0tbw&usqp=CAU",
+      "url": "https://www.volvocars.com/en-ca/cars/s90-hybrid/",
+      "shop": "https://www.volvocars.com/us/v/cars/s90-hybrid/shop"
+    },
+    {
+      "id": "s60-recharge",
+      "modelName": "S60 Recharge", 
+      "bodyType": "sedan",
+      "modelType": "plug-in hybrid",
+      "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXldcG_mTdv9L6U_T5kNdggRCtuaAJwbGF_A&usqp=CAU",
+      "url": "https://www.volvocars.com/en-ca/cars/s60-hybrid/",
+      "shop": "https://www.volvocars.com/us/v/cars/s60-hybrid/shop"
+    }
+]
